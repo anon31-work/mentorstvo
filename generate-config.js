cf09b8c00e70efe2b9866f2799b4d5075c9adfc1
anon31-work <justtestingsomething31@gmail.com>
2026-04-21 13:30:04 +0200

    f

--- generate-config.js
+++ generate-config.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 const configJs = `const CONFIG = {
-    SUPABASE_URL: "${process.env.SUPABASE_URL || 'https://oswxhbcqcvtlkvqegmvv.supabase.co'}",
-    SUPABASE_ANON_KEY: "${process.env.SUPABASE_ANON_KEY || 'sb_publishable_peMqjUkPKw3XYwO1n5otOw_G82_9ioP'}"
+    SUPABASE_URL: "${process.env.SUPABASE_URL}",
+    SUPABASE_ANON_KEY: "${process.env.SUPABASE_ANON_KEY}"
 };`;
 
 fs.writeFileSync('config.js', configJs);
